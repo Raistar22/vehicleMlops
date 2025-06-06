69f7489cbc8de3b8188bd2c7eb123c834c072b31
Ritik Kumar <ritik.kumar@neocubelabs.com>
2025-06-06 12:02:28 +0530

    initial

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Ritik Kumar",
+    author_email="yesiamritik33@gmail.com",
+    packages=find_packages(),    
+)
